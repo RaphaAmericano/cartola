28b27758b9c792eefd462e87edf5fadbd2015430
RaphaAmericano <raphael@raphaelamericano.com.br>
2018-04-19 22:56:26 -0300

    Dados impressos corretamente

--- cartola.php
+++ cartola.php
@@ -2,17 +2,20 @@
 
 <?php
 include('header.php'); 
-class Cartola { 
+class Cartola {
+
+    private $host = "127.0.0.1";
+    private $db = "cartola";
+    private $user = "root";
+    private $pass = "";
 
     function __construct() {
-        $host = "127.0.0.1";
-        $db = "cartola";
-        $user = "root";
-        $pass = "";
-    
-        $con = new mysqli($host, $user, $pass, $db); 
-        //var_dump($con);
-    
+        Cartola::listarRodadas();
+        Cartola::listarLideranca();
+    }
+
+    public function listarRodadas(){
+        $con = new mysqli($this->host,  $this->user, $this->pass, $this->db);   
         if ($con->connect_errno) {
             print 'Erro '.$con->connect_error.'';
             exit;
@@ -60,6 +63,61 @@ class Cartola {
                 <td><?php print $row['NOME_EQUIPE']; ?></td>
                 <td><?php print $row['NOME_JOGADOR']; ?></td>
                 <td><?php print $row['TOTAL_PONTOS']; ?></td>
+            </tr>
+            
+            <?php }
+
+            print '</tbody></table>';
+            $resultado->close();
+        }
+
+        // $resultado->free();
+        $con->close();
+    }
+
+    public function listarLideranca(){
+        $con = new mysqli($this->host,  $this->user, $this->pass, $this->db);   
+        if ($con->connect_errno) {
+            print 'Erro '.$con->connect_error.'';
+            exit;
+        };
+
+        $query_lista_rodadas = sprintf("SELECT 
+        E.NOME_EQUIPE, sum(P.TOTAL_PONTOS) AS TOTAL
+        FROM 
+        EQUIPE E INNER JOIN PONTUACAO P ON E.ID_JOGADOR = P.ID_JOGADOR
+        INNER JOIN RODADA R ON R.ID_RODADA = P.ID_RODADA
+        GROUP BY  
+        E.NOME_EQUIPE");
+
+        if(!$resultado = $con->query($query_lista_rodadas)){
+            print 'Erro '. $con->error .'\n' ;
+            print "Errno: " . $con->errno . "\n";;
+            exit;
+        }
+
+        if($resultado->num_rows === 0 ){
+            print "0";
+            exit;
+        }
+        if($resultado = $con->query($query_lista_rodadas)){
+            ?>
+            <table class="table">
+            <thead>
+                <tr>
+                    <th>Posição</th>
+                    <th>Equipe</th>
+                    <th>Total de Pontos</th>
+                </tr>
+            </thead>
+            <tbody>
+            <?php
+            $c = 1;
+            while ($row = $resultado->fetch_assoc()) { ?>
+            <tr>
+                <td><?php print $c; $c++; ?></td>
+                <td><?php print $row['NOME_EQUIPE']; ?></td>
+                <td><?php print $row['TOTAL']; ?></td>
             </tr>
             
             <?php }
